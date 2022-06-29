b443067665ebedb285da0d51f9fd24bc9503707a
wendhsz20 <7076862+wendhsz@users.noreply.github.com>
2022-06-29 14:08:18 +0800

    forgot to uncomment :/

--- app/src/main/java/com/janefab20/onlineshop/MainActivity.java
+++ app/src/main/java/com/janefab20/onlineshop/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
 
 
         setContentView(R.layout.activity_main);
